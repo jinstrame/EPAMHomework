540cb5b9ae977c9a85dc6c5e847bd5783a161776
Eugene <jinstrame@gmail.com>
2016-09-04 17:01:33 +0300

    completed Task1 on module Intro

---
+++ intro/src/main/java/compileJavaSE_T01.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Java\jdk1.8.0_102\bin\javac.exe"
+"C:\Program Files\Java\jdk1.8.0_102\bin\javac.exe" -d javase01\t01\bin\ javase01\t01\logic\Logic.java
+"C:\Program Files\Java\jdk1.8.0_102\bin\javac.exe" -d javase01\t01\bin\ -cp javase01\t01\bin\ javase01\t01\main\Main.java
+cd javase01\t01\bin\
+java -cp . javase01.t01.main.Main
---
+++ intro/src/main/java/javase01/t01/logic/Logic.java
@@ -0,0 +1,10 @@
+package javase01.t01.logic;
+
+/**
+ * Created by Eugene on 04.09.2016.
+ */
+public class Logic {
+    public String method(){
+        return "I am String in Logic";
+    }
+}
