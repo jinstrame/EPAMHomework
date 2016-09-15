1023e50d3b77e93a400cfdf411e31b39c77714f6
Eugene <jinstrame@gmail.com>
2016-09-15 03:23:18 +0300

    edited test for task 1 in 3rd module (strings)

--- strings/src/test/java/strings/task01/CrazyLoggerTest.java
+++ strings/src/test/java/strings/task01/CrazyLoggerTest.java
@@ -3,6 +3,12 @@ package strings.task01;
 import org.junit.Test;
 
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class CrazyLoggerTest {
     @Test
@@ -10,7 +16,12 @@ public class CrazyLoggerTest {
         CrazyLogger.log("message1");
         CrazyLogger.log("message to message");
         Iterator<String> i = CrazyLogger.search("message\\w");
-        while (i.hasNext())
-            System.out.println(i.next());
+
+        String s = i.next();
+        Pattern p = Pattern.compile("([0-9]{2}\\-){2}[0-9]{4} : [0-9]{2}\\-[0-9]{2} \\- .*");
+        Matcher m = p.matcher(s);
+
+        assertTrue(m.find());
+        assertThat(m.end() - m.start(), is(s.length()));
     }
 }
